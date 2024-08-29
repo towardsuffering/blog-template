82fce6f13d5c94df5e6bff0f1f1bd8a9932d7f0a
Chris <christopher.rotunno@outlook.com>
2024-08-28 23:53:47 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://superlative-faloodeh-1a5492.netlify.app/', // Write here your website url
+	site: 'https://chrisblogs.dev', // Write here your website url
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 		drafts: true,
